11262eb339eeb5b8af0b8e278489486c2c537deb
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-02-06 04:08:09 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Employee Management',
+    'version' : '1.0',
+    'summary':'',
+    'sequence': 1,
+    'description':""" Buat Daftar Sekolah """,
+    'category':'Human Resources',
+    'website':'google.com',
+    'license': 'LGPL-3',
+    'depends':[
+    ],
+
+    'data':[
+
+    ],
+    'demo':[],
+    'qweb':[],
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+}
